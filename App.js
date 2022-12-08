1514e3b97c34f7316e60f23ce8476ef8cf2539de
Karly Lamm <klamm@voltsystems.net>
2022-12-07 18:50:18 -0600

    can add and delete items

--- App.js
+++ App.js
@@ -1,17 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Keyboard, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import KeyboardAvoidingView from 'react-native/Libraries/Components/Keyboard/KeyboardAvoidingView';
 import Task from './components/Task';
 
 export default function App() {
+  const[task, setTask] = useState();
+  const[taskItems, setTaskItems] = useState([]);
+
+
+  const handleAddTask = () => {
+    Keyboard.dismiss();
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) =>{
+    let itemsCopy = [...taskItems];
+
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
   return (
     <View style={styles.container}>
     <View style={styles.tasksWrapper}>
      <Text style={styles.sectionTitle}>Today's Text</Text>
      <View style={styles.items}>
-        <Task text={"something"}></Task>
+      {
+        taskItems.map((item, index)=>{
+            return(
+            <TouchableOpacity key={index} onPress={()=> completeTask(index)}>
+              <Task text={item}/>
+            </TouchableOpacity>
+          )
+        })
+      }
      </View>
     </View>
 
+    <KeyboardAvoidingView
+    behavior={Platform.OS=== "ios" ? 'padding' : 'height'}
+    style={styles.writeTaskWrapper}>
+      <TextInput
+      style={styles.input}
+      placeholder={"Write a task"}
+      value={task}
+      onChangeText={text => setTask(text)}/>
+      <TouchableOpacity onPress={() => handleAddTask()}>
+        <View style={styles.addWrapper}>
+          <Text style={styles.addText}>+</Text>
+        </View>
+      </TouchableOpacity>
+    </KeyboardAvoidingView>
     </View>
   );
 }
@@ -32,4 +71,32 @@ const styles = StyleSheet.create({
     items:{
       marginTop: 30
     },
+    writeTaskWrapper:{
+      position: 'absolute',
+      bottom: 60,
+      width: '100%',
+      flexDirection:'row',
+      justifyContent:'space-around',
+      alignItems:'center'
+    },
+    input:{
+      paddingVertical: 15,
+      width: 250,
+      paddingHorizontal: 15,
+      backgroundColor: '#FFF',
+      borderRadius: 60,
+      borderColor:'#C0C0C0',
+      borderWidth: 1
+    },
+    addWrapper:{
+      width: 60,
+      height: 60,
+      backgroundColor: '#FFF',
+      borderRadius: 60,
+      justifyContent:'center',
+      alignItems:'center',
+      borderColor:'#C0C0C0',
+      borderWidth: 1
+    },
+    addText:{},
 });
